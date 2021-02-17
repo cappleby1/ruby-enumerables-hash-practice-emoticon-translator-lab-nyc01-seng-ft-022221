c9b1cc628e36db87593e966a81f7c293e9e286f1
Colten Appleby <colten96@gmail.com>
2021-02-17 05:43:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,43 @@
 # require modules here
 
-def load_library
+require "yaml"
+require "pry"
+
+
+def load_library(location)
   # code goes here
+  
+  emojis = YAML.load_file(location)
+  
+  return emojis
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(eng)
   # code goes here
+  
+  emojis.each do |key, symbols|
+    if symbols[0] == eng
+      return key
+    end
+  end
+      
+  
+  
 end
 
-def get_english_meaning
+def get_english_meaning(jap)
   # code goes here
+  
+  emojis.each do |key, value|
+    
+  end
+  
+  
 end
+
+
+#location = './lib/emoticons.yml'
+#data = load_library(location)
+
+
+#binding.pry
